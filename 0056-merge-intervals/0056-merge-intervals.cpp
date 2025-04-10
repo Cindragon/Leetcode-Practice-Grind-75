ca1abe8ba3106ec4ce7879c3816c85083c6b7c35
Cindragon <114350612+Cindragon@users.noreply.github.com>
2025-04-10 15:11:08 +0800

    Update 0056-merge-intervals.cpp

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -17,3 +17,14 @@ public:
         return merged;
     }
 };
+
+/*
+要對區間先進行排序
+用temp來存一開始的區間起始值
+接下來用迴圈遍歷區間( auto it
+如果元素[0]小於或等於temp[1]
+那代表可以進行合併(用max來比it[1]跟temp[1]的大小)
+或者不重複 那將temp push_back到merge interval
+temp 等於it 後繼續比較
+最後的temp也要記得加入到merge裡面。
+*/
