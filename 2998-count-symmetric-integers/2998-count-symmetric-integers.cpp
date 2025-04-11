26f17e91e340d200ae1949d828bc5d17f1c2be19
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-04-11 21:53:16 +0800

    Time: 69 ms (55.8%), Space: 9.8 MB (56.7%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -8,11 +8,16 @@ public:
             if(n%2!=0) continue;
             int mid=n/2, left=0, right=0;
             for(int i=0;i<mid;i++){
-                left+=s[i];
-                right+=s[i+mid];
+                left+=s[i]-'0';
+                right+=s[i+mid]-'0';
             }
             if(left==right) ans++;
         }
         return ans;
     }
 };
+
+/*
+計算時要記得-'0'才是數字加總
+不然會變成 ASCII 值相加(就算結果對但是意義不一樣)
+*/
