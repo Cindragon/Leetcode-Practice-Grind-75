0811d0225d4cdac55cb5cacd45b2f01939e21ce2
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-04-12 17:58:23 +0800

    Time: 0 ms (100%), Space: 14.6 MB (18.06%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -16,3 +16,33 @@ public:
 接著遍歷 nums2，如果某個元素也存在於 nums1_set 中，就將其加入結果集合 ans（自動去重）
 最後將結果集合轉換成 vector 並回傳
 */
+
+/*
+另一種寫法
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int m=nums1.size(), n=nums2.size();
+        int i=0, j=0;
+        while(i<m&&j<n){
+            if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums2[j]>nums1[i]){
+                i++;
+            }
+            else{
+                if(ans.empty()||ans.back()!=nums1[i]){
+                    ans.push_back(nums1[i]);
+                }
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
+*/
