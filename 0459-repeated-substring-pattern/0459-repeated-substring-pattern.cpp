71a94742b4fa4e07e138ffa66b2d21a35e76c693
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-04-20 16:03:04 +0800

    Time: 0 ms (100%), Space: 11.6 MB (99.38%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void getNext(const string&s, int* next){
+        next[0]=0;
+        int j=0;
+        for(int i=1;i<s.size();i++){
+            while(j>0 && s[i]!=s[j]){
+                j=next[j-1];
+            }
+            if(s[i]==s[j]){
+                j++;
+            }
+            next[i]=j;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+        if(s.size()==0) return false;
+        int next[s.size()];
+        getNext(s, next);
+        int len=s.size();
+        if(next[len-1]!=0 &&(len%(len-next[len-1]))==0){
+            return true;
+        }
+        else
+            return false;
+    }
+};
