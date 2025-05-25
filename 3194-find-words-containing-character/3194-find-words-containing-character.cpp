1a25da23e2590dfa2aaaddcd066e6c86d2df69f1
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-05-25 22:33:09 +0800

    Time: 0 ms (100%), Space: 33 MB (56.16%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -3,9 +3,10 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> res;
         for(int i=0;i<words.size();i++){
-            string s=words[i];
-            for(int j=0;j<s.size();j++){
-                if(s[j]==x){
+            bool flag=0;
+            for(const auto&c:words[i]){
+                if(c==x){
+                    flag=1;
                     res.push_back(i);
                     break;
                 }
