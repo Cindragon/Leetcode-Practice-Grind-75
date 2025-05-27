ae7abd92ad5f77d7e184ef8efac147d5823c75dc
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-05-28 02:20:28 +0800

    Time: 0 ms (100%), Space: 31.3 MB (74.1%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -13,6 +13,27 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
+        TreeNode*left=root->left;
+        TreeNode*right=root->right;
+        int leftDepth=0, rightDepth=0;
+        while(left){
+            left=left->left;
+            leftDepth++;
+        }
+        while(right){
+            right=right->right;
+            rightDepth++;
+        }
+        if(leftDepth==rightDepth) (2<<leftDepth)-1;;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
+/*
+用後序遍歷來算樹節點
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
         int count=0;
         int left=countNodes(root->left);
         int right=countNodes(root->right);
@@ -20,6 +41,9 @@ public:
         return count;
     }
 };
-/*
-用後序遍歷來算樹節點
+
+或是用完全二叉樹的特性(節點數為2^層數-1)
+計算左子樹跟右子樹的深度
+如果深度一樣，那麼節點的個數就是 2<<深度-1
+最後再將左右子樹的個數相加再+1
 */
