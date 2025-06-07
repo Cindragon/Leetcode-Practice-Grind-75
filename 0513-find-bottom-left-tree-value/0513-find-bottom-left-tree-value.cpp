c22444ca7a66785fd263a089746eaf4d40f94e00
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-06-07 22:46:15 +0800

    Time: 0 ms (100%), Space: 25 MB (64.42%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,6 +11,51 @@
  */
 class Solution {
 public:
+    int maxDepth=INT_MIN;
+    int result;
+    void Traversal(TreeNode* root, int depth){
+        if(root==NULL) return;
+        if(root->left==NULL&&root->right==NULL){
+            if(depth>maxDepth){
+                maxDepth=depth;
+                result=root->val;
+            }
+            return;
+        }
+        if(root->left){ //=Traversal(root->left, depth+1);
+            depth++;
+            Traversal(root->left, depth);
+            depth--;
+        }
+        if(root->right){ //=Traversal(root->right, depth+1);
+            depth++;
+            Traversal(root->right, depth);
+            depth--;
+        }
+        return;
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        Traversal(root, 0);
+        return result;
+    }
+};
+
+/*
+先定義兩個全域變數(maxdepth最大深度, result是要更新的最左下角的值)
+用遞迴法來寫，用前中後序都可以
+因為最重要的是在深度最大的時候，找到最左下角的值
+終止條件為
+找到葉子節點的時候，如果depth>maxdepth，要更新深度，並且result會等於這個節點的值
+向左跟向右遞迴時要記得回溯，才能正確更新深度
+最後回傳result
+*/
+
+/*
+另一種是用層次遍歷(BFS)的方式來寫
+只要找到每一層最左邊的值就好
+最後再回傳
+class Solution {
+public:
     int findBottomLeftValue(TreeNode* root) {
         queue<TreeNode*> que;
         if(root==NULL) return 0;
@@ -29,9 +74,4 @@ public:
         return result;
     }
 };
-
-/*
-用層次遍歷的方式來寫
-只要找到每一層最左邊的值就好
-最後再回傳
 */
