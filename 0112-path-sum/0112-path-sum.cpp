73ec324fad651902c6ad785492d7fd08c8fc1961
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-06-08 23:02:23 +0800

    Time: 0 ms (100%), Space: 21.6 MB (34.57%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -15,7 +15,6 @@ public:
         if(root==NULL) return 0;
         if(root->left==NULL && root->right==NULL){
             if(targetSum==root->val) return 1;
-            else return 0;
         }
         bool left, right;
         left=hasPathSum(root->left, targetSum-root->val);
@@ -23,3 +22,12 @@ public:
         return left||right;
     }
 };
+/*
+用遞迴法的方式
+使用前序遍歷(中左右)
+檢查如果是到葉子節點時，如果targetSum等於root的值(相減之後為0)
+代表有一條路徑相加之後可以找到這個target
+接著左右子樹遞迴
+每次都傳入target-root->val的值
+最後return left||right(其中有一條是true就可以)
+*/
