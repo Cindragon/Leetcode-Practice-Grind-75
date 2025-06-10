1c38fd28f8d0907bca112d50536574b31a663688
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-06-11 02:21:27 +0800

    Time: 0 ms (100%), Space: 33.1 MB (89.69%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -14,19 +14,16 @@ public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         if(root1==NULL) return root2;
         if(root2==NULL) return root1;
-        TreeNode* root=new TreeNode();
-        if(root1!=NULL&&root2!=NULL){   //中
-            root->val=root1->val+root2->val;
-        }
-        root->left=mergeTrees(root1->left, root2->left);    //左
-        root->right=mergeTrees(root1->right, root2->right); //右
-        return root;
+        root1->val+=root2->val; //中
+        root1->left=mergeTrees(root1->left, root2->left);    //左
+        root1->right=mergeTrees(root1->right, root2->right); //右
+        return root1;
     }
 };
 /*
-使用前序遍歷來合併二元樹(中左右)
+使用前序遍歷來合併二元樹(中左右)，並且直接修改root1
 終止條件為 如果遇到root1==NULL 回傳root2，反之亦然(因為兩棵樹的順序是一起遍歷的)
-接著處理中，如果root1跟root2都不為NULL root的值等於兩者相加
+接著處理中，如果root1跟root2都不為NULL root1的值等於roo1+root2
 然後左右子樹就再用遞迴的方式來建構
-最後回傳root
+最後回傳root1(因為我們已經直接修改了)
 */
