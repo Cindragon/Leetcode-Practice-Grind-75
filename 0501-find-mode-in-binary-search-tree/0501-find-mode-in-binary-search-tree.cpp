c9002af9ed99f8201d4a9d72ae2dc007bd02173f
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-06-15 18:24:52 +0800

    Time: 0 ms (100%), Space: 24.4 MB (77.01%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,13 +11,14 @@
  */
 class Solution {
 private:
-    TreeNode* pre;
+    TreeNode* pre=NULL;
     vector<int> res;
     int count=0;
     int maxCount=0;
     void Traversal(TreeNode* root){
         if(root==NULL) return;
-        Traversal(root->left);
+        Traversal(root->left);//左
+        //中
         if(pre==NULL) count=1;
         else if(pre->val==root->val) count++;
         else count=1;
@@ -30,8 +31,8 @@ private:
             maxCount=count;
         }
         pre=root;
-
-        Traversal(root->right);
+        //中
+        Traversal(root->right); //右
     }
 
 public:
@@ -40,3 +41,17 @@ public:
         return res;
     }
 };
+
+/*
+BST就一樣用中序遍歷
+首先定義前一個節點、res陣列加上記數count跟最大記數maxCount
+如果root==NULL return 空陣列
+如果pre==NULL 先讓count=1
+或是如果 pre跟root的值相等 count++
+又或是(值不相等) count重設為1(重新計算)
+然後將count跟maxCount做比較
+如果count==maxCount 把root->val加到res(因為眾數可能不只一個)
+如果count更大(代表找到另一個眾數)
+將陣列清空，重新加入並且更新maxCount的數字
+最後回傳res
+*/
