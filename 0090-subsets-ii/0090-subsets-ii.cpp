c6129318868e733c351f36704626091885e20f10
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-01 00:47:20 +0800

    Time: 3 ms (38.21%), Space: 10.8 MB (39.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -6,7 +6,7 @@ public:
         res.push_back(path);
         if(startIndex>=nums.size()) return;
         for(int i=startIndex;i<nums.size();i++){
-            if(i>startIndex&&nums[i]==nums[i-1]){
+            if(i>startIndex&&nums[i]==nums[i-1]){   //對使用過的元素進行跳過(同層去重)
                 continue;
             }
             path.push_back(nums[i]);
@@ -23,3 +23,17 @@ public:
         return res;
     }
 };
+
+/*
+由於子集內可以有重複的元素
+因此需要去重，在加入子集之前先進行排序
+然後就跟子集的問題一樣
+使用回溯算法解決
+傳入參數：nums數組跟startIndex
+將path加入res中
+單層遞迴邏輯：
+    需要將重複的元素去除(注意這只會跳過「同一層」的重複值，下一層仍然會使用該數)
+    接著一樣將nums[i]加入path
+    進行遞迴之後，最後進行回溯
+最後回傳res
+*/
