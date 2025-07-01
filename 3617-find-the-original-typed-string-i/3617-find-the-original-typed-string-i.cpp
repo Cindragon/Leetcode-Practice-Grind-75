f5ef26f6f149bc69feffbf1f436e7bb1f203bb2e
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-01 10:28:59 +0800

    Time: 0 ms (100%), Space: 8.8 MB (60.22%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int possibleStringCount(string word) {
         int ans=1;
-        for(int i=1;i<word.size();i++){
-            if(word[i]==word[i-1])  ans++;
+        for(int i=0;i<word.size();i++){
+            if(word[i]==word[i+1])  ans++;
         }
         return ans;
     }
