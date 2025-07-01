b22251c282f8db88e86af0ea2f134fe193153b91
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-02 03:15:13 +0800

    Time: 0 ms (100%), Space: 22 MB (46.61%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,6 +11,29 @@
  */
 class Solution {
 public:
+    TreeNode* pre=NULL;
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL) return true;
+        bool left=isValidBST(root->left);
+        if(pre!=NULL&&pre->val>=root->val) return false;
+        pre=root;
+        bool right=isValidBST(root->right);
+        return left&&right;
+    }
+};
+
+/*
+遞迴法用中序遍歷的方式來處理
+首先定義一個最小值maxValue(long long型態)
+判斷終止條件 如果root==NULL return true
+接著是遍歷左邊
+中間的值如果比maxValue大就用root的值取代
+不然就回傳false
+最後遍歷右邊
+然後回傳 left&&right(都是true就代表是BST)
+
+class Solution {
+public:
     long long maxValue=LONG_MIN;
     bool isValidBST(TreeNode* root) {
         if(root==NULL) return true;
@@ -24,3 +47,8 @@ public:
         return left&&right;
     }
 };
+
+另一種解法就是儲存前一個節點的值
+然後再進行比較
+如果pre!=NULL&&pre的值大於等於root的值(說明不是BST) 回傳false
+*/
