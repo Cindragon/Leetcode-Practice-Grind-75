22004e3c07d727bf08f87ee490bb46605042c376
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-02 20:43:57 +0800

    Time: 0 ms (100%), Space: 9.9 MB (87.51%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int res=1;
+        int prediff=0;
+        int curdiff=0;
+        for(int i=0;i<nums.size()-1;i++){
+            curdiff=nums[i+1]-nums[i];
+            if((prediff>=0&&curdiff<0)||(prediff<=0&&curdiff>0)){
+                res++;
+                prediff=curdiff;
+            }
+        }
+        return res;
+    }
+};
