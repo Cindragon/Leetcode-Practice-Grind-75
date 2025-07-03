9bf61d7cb378416ed082ff8120406ce112f898e3
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-03 13:03:41 +0800

    Time: 0 ms (100%), Space: 71.7 MB (80.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=nums[0];
-        int res=sum;
-        for(int i=1;i<nums.size();i++){
-            if(sum<0) sum=0;
+        int sum=0;
+        int res=INT_MIN;
+        for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            res=max(res, sum);
+            if(sum>res) res=sum;
+            if(sum<0) sum=0;
         }
         return res;
     }
@@ -14,6 +14,7 @@ public:
 
 /*
 這局的貪心算法邏輯是：如果目前的連續和小於0，那乾脆就從下一個數開始重頭加總
-因此在 for 迴圈中進行判斷，如果sum<0直接重新開始
+因此在 for 迴圈中進行判斷，將sum進行加總
 接著跟 res 做比較儲存最大值，最後回傳res
+如果sum<0直接重新開始
 */
