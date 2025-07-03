f35ce5a8905a1941e5c524124c897d83ae872e51
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-03 14:36:59 +0800

    Time: 4 ms (24.42%), Space: 52.4 MB (33.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -10,3 +10,12 @@ public:
         return false;
     }
 };
+
+/*
+一樣是使用貪心算法的方式，只要求能夠覆蓋到終點的最大範圍就好
+設一個變量 cover，目前能跳到的最遠位置，接著透過迴圈來更新cover的範圍
+cover=max(i+nums[i], cover)
+意思是：從位置 i 最遠能跳到 i + nums[i]，我們要看是否能讓 cover 向右延伸。
+如果大於等於數組範圍就 return true
+不然就 return false
+*/
