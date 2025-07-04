9ace0e6a09854e1e614a0667beecebf9acb38bf8
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-04 14:51:04 +0800

    Time: 0 ms (100%), Space: 12.6 MB (79.62%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
@@ -1,24 +1,28 @@
 class Solution {
+static bool cmp(int a, int b){
+    return abs(a)>abs(b);
+}
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        int sum=0;
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size()&&k>0;i++){
-            if(nums[i]<0){
-                nums[i]=-nums[i];
+        sort(nums.begin(), nums.end(), cmp);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0&&k>0){
+                nums[i]*=-1;
                 k--;
             }
         }
-        sort(nums.begin(), nums.end());
-        
-        if(k%2==1){
-            nums[0]=-nums[0];
-        }
-
+        if(k%2==1) nums[nums.size()-1]*=-1;
+        int sum=0;
         for(const auto&i:nums){
-            cout<<i<<" ";
             sum+=i;
         }
         return sum;
     }
 };
+
+/*
+使用 sort 加上自定義的比較函數來對數組進行絕對值的排列
+接著開始對小於0的數字進行反轉
+最後若是沒有用完反轉次數，就把數組最小的正數進行反轉
+最後回傳總和
+*/
