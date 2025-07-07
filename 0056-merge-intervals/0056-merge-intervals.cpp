71150d7b8e2cc0feaf4306ffc921914ee7d509aa
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-08 00:51:15 +0800

    Time: 4 ms (69.28%), Space: 23.9 MB (31.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> merged;
-        if(intervals.size()==0) return merged;
         sort(intervals.begin(), intervals.end());
-        vector<int> temps=intervals[0];
-        for(const auto& it:intervals){
-            if(it[0]<=temps[1]){
-                temps[1]=max(it[1], temps[1]);
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<= merged.back()[1]){
+                merged.back()[1]=max(merged.back()[1], intervals[i][1]);
             }
             else{
-                merged.push_back(temps);
-                temps=it;
+                merged.push_back(intervals[i]);
             }
         }
-        merged.push_back(temps);
         return merged;
     }
 };
+
 /*
-要對區間先進行排序
-用temp來存一開始的區間起始值
-接下來用迴圈遍歷區間( auto it
-如果元素[0]小於或等於temp[1]
-那代表可以進行合併(用max來比it[1]跟temp[1]的大小)
-或者不重複 那將temp push_back到merge interval
-temp 等於it 後繼續比較
-最後的temp也要記得加入到merge裡面。
+一樣用貪心算法，找到重疊的區間之後
+若是區間有重疊的部分就進行合併，不然就加入新的區間
+最後回傳合併過後的區間
 */
