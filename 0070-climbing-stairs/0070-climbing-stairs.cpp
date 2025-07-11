70b7eb94859bd4b0d073ffbd9f935912f5759246
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-11 22:25:16 +0800

    Time: 0 ms (100%), Space: 7.9 MB (72.81%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1) return n;
+        int dp[2];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int sum=dp[0]+dp[1];
+            dp[0]=dp[1];
+            dp[1]=sum;
+        }
+        return dp[1];
+    }
+};
