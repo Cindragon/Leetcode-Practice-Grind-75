5ee81bb623dc53e84e8efab3f31786336e485cbf
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-21 20:39:18 +0800

    Time: 3 ms (95.29%), Space: 10.2 MB (91.99%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -18,5 +18,5 @@ dp 數組定義：達到總額為 j 的硬幣組合有 dp[j] 種
 遞推公式：dp[j]+=dp[j-coins[i]](放了這個硬幣跟不放這個硬幣的組合加總)
 初始化：dp[0]=1
 遍歷順序：先遍歷物品再遍歷背包，而且背包大小採正序遍歷(小到大)
-最後回傳dp[amount]
+最後回傳dp[amount](用double或是 unsigned long long 都可以)
 */
