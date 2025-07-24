1a1d2a26efc0aa664dfe9f63d59c53359d33db5a
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-24 17:26:53 +0800

    Time: 79 ms (72.3%), Space: 89.5 MB (31.91%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -8,8 +8,8 @@ public:
             for(int j=1;j<=len2;j++){
                 if(nums1[i-1]==nums2[j-1]){
                     dp[i][j]=dp[i-1][j-1]+1;
+                    res=max(res, dp[i][j]);
                 }
-                if(res<dp[i][j])    res=dp[i][j];
             }
         }
         return res;
