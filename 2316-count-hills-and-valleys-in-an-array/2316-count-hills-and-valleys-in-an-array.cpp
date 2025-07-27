833d7c03656d72fe66cb012984510c797aa14e3e
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-07-27 21:44:00 +0800

    Time: 0 ms (100%), Space: 12.2 MB (19.85%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -9,13 +9,18 @@ public:
         arr.push_back(nums[nums.size()-1]);
         int res=0;
         for(int i=1;i<arr.size()-1;i++){
-            if(arr[i-1]<arr[i]&&arr[i]>arr[i+1]){
+            if(arr[i-1]<arr[i]&&arr[i]>arr[i+1]){   //計算山峰
                 res++;
             }
-            else if(arr[i-1]>arr[i]&&arr[i]<arr[i+1]){
+            else if(arr[i-1]>arr[i]&&arr[i]<arr[i+1]){  //計算山谷
                 res++;
             }
         }
         return res;
     }
 };
+/*
+先將重複的值剃除(避免影響計算結果)
+接著將值進行比較，並且將山峰跟山谷的數量進行加總
+最後回傳 res
+*/
