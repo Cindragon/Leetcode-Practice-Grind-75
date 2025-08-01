c6216fbd3fe018f2df12d6c1a1be1d38cbddd8bf
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-01 15:09:52 +0800

    Time: 0 ms (100%), Space: 14.8 MB (13.51%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> temp=nums;
         vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            int count=0;
-            for(int j=0;j<nums.size();j++){
-                if(nums[i]>nums[j]){
-                    count++;
-                }
+        sort(temp.begin(), temp.end());
+
+        unordered_map<int, int> mp;
+        for(int i=0;i<temp.size();i++){
+            if(mp.find(temp[i])==mp.end()){
+                mp[temp[i]]=i;
             }
-            res.push_back(count);
+        }
+        for(const auto&n:nums){
+            res.push_back(mp[n]);
         }
         return res;
     }
 };
+
+/*
+除了暴力解還可以用 map 來找
+先透過 temp 暫存原本的 nums 數組，並且進行排序
+接著用 map 來儲存各個數值在 temp 數組裡的位置(代表有幾個數字比他大)
+重複的數字就跳過(因此要從前往後遍歷)
+最後將 mp 的值傳入 res
+*/
