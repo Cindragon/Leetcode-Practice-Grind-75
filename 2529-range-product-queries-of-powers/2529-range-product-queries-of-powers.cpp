a297c77925bbc18df556dde2d052a5c43612a6de
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-11 20:06:49 +0800

    Time: 23 ms (54.79%), Space: 147.5 MB (77.13%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int MOD = 1000000007;
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> powers;
+        int power = 1;
+        while (n > 0) {
+            if (n & 1) {
+                powers.push_back(power);
+            }
+            power <<= 1;
+            n >>= 1;
+        }
+        vector<int> result;
+        for(int i = 0; i<queries.size(); i++){
+            int p = 1;
+            for(int j = queries[i][0]; j<=queries[i][1]; j++){
+                p = ((long long)p*powers[j])%MOD;
+            }
+            result.push_back(p);
+        }
+        return result;
+    }
+};
+
+/*
+先用 power 陣列將 n 拆成 2 的冪
+接著逐個計算 queries 的區間乘積，最後把結果加入到result中
+*/
