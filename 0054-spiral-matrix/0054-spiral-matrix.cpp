fdf0dff137bd56ca82bdd60ef8a6f4d4cc450c09
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-13 16:20:04 +0800

    Time: 0 ms (100%), Space: 9.4 MB (12.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
-        int m=matrix.size(), n=matrix[0].size();
-        int left=0,bottom=m-1, top=0, right=n-1;
-        while(left<=right && top<=bottom){
-            for(int j=left;j<=right;j++){
-                res.push_back(matrix[top][j]);
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int top=0, bottom=m-1, left=0, right=n-1;
+        while(top<=bottom&&left<=right){
+            for(int i=left;i<=right;i++){
+                res.push_back(matrix[top][i]);
             }
             top++;
-            for(int i=top;i<=bottom;i++){
-                res.push_back(matrix[i][right]);
-            }
-            right--;
             if(top<=bottom){
-                for(int j=right;j>=left;j--){
-                    res.push_back(matrix[bottom][j]);
+                for(int i=top;i<=bottom;i++){
+                    res.push_back(matrix[i][right]);
+                }
+                right--;
+            }
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
@@ -29,3 +32,13 @@ public:
         return res;
     }
 };
+
+/*
+用螺旋順序輸出陣列的各個數，最重要的是邊界處理的部分(左閉右閉的方式)
+以四個變數分別代表上下左右
+先從左到右輸出，輸出完畢後將 top++(最上面這排輸出完畢)
+接著從上到下，輸出完畢後一樣將 right--(最右邊已輸出完畢)
+從右到左，需要注意是在 top<=bottom 的情況下，不然有可能重複輸出數字(最後bottom--)
+最後再下到上，一樣需要注意是在 left<=right，最後 left++
+結束後回傳 res
+*/
