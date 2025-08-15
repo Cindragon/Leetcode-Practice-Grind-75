c07535494dc5b185c32dfdeed64d84fab4dfe492
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-15 14:11:51 +0800

    Time: 0 ms (100%), Space: 15 MB (24.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,23 +15,16 @@ public:
         dummyHead->next=head;
         ListNode* fast=dummyHead;
         ListNode* slow=dummyHead;
-        for(int i=0;i<n+1;i++){
-            fast=fast->next;
+        while(n--&&fast->next!=NULL){
+            fast=fast->next;    
         }
+        fast=fast->next;    //fast 再多走一步，slow 最後會走到要刪除的前一個節點
         while(fast!=NULL){
             fast=fast->next;
             slow=slow->next;
         }
-        ListNode* del=slow->next;
-        slow->next=del->next;
-        delete del;
+        slow->next=slow->next->next;
         return dummyHead->next;
+
     }
 };
-
-/*
-用快慢指針
-快指針多走n+1步
-這樣在快指針指向節點時
-慢指針剛好指向要刪除的前一個節點
-*/
