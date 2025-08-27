5f585cb59616d51051e01cc497c9777449b84cee
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-27 16:46:09 +0800

    Time: 0 ms (100%), Space: 9.8 MB (48.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
+        if(s.length()!=t.length())  return false;
         int alphacount[26];
-        for(const auto &c:s){
+        for(const auto&c:s){
             alphacount[c-'a']++;
         }
-        for(const auto &c:t){
+        for(const auto&c:t){
             alphacount[c-'a']--;
         }
-        for(const auto &i:alphacount){
-            if(i!=0) return false;
+        for(const auto&i:alphacount){
+            if(i!=0)    return false;
         }
         return true;
     }
 };
 /*
-用一個26格陣列來計算第一個字串每個字母出現的次數
-再遍歷第二個字串有重複的減減
-最後計算字串中如果有非0的值就return false(代表有多出字母)
+因為只由小寫字母組成，因此用 array[26]便能計算完畢
+將兩個字串中的字母進行數量計算，計算之後如果alphacount裡面有大於0的數
+代表兩個字串不是相同字母異序詞(anagram)
 */
