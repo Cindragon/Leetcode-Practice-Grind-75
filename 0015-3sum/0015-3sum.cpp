2f668c28eca7d0d44f107086aeb09fd2ac866484
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-28 15:52:43 +0800

    Time: 47 ms (70.04%), Space: 29 MB (72%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-
-        int i, j, k, sum;
-        for(i=0;i<nums.size();i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0)   return res;
+            if(i>0 && nums[i]==nums[i-1])    continue;
             int left=i+1, right=nums.size()-1;
             while(left<right){
-                sum=nums[i]+nums[left]+nums[right];
-                int target=0;
-                if(sum==target){
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    // 去重 left，確保 left 不重複
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    // 去重 right，確保 right 不重複
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                if(nums[i]+nums[left]+nums[right]>0)    right--;
+                else if(nums[i]+nums[left]+nums[right]<0)   left++;
+                else{
+                    res.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    while(right>left && nums[left]==nums[left+1]) left++;
+                    while(right>left && nums[right]==nums[right-1])   right--;
                     left++;
                     right--;
                 }
-
-                else if(sum>target){
-                    right--;
-                }
-                else left++;
             }
         }
-        return ans;
+        return res;
     }
 };
+
+/*
+一開始先進行排序，透過雙指針的方式來求解
+接著先鎖定第一個數，並且要進行剪枝(因為不能重複)
+接著開始設定雙指針的左右，在left<right的情況下
+如果三個數相加>0 right--，如果<0 left++，如果相加為0就加入res
+找到一組之後左右也需要剪枝(因為有可能有重複的數)
+最後回傳res
+*/
