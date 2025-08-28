a6707e73b8e29dc9e536cf5bc0ad27f643309829
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-28 13:52:33 +0800

    Time: 0 ms (100%), Space: 8.4 MB (41.03%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -9,18 +9,16 @@ public:
         return sum;
     }
     bool isHappy(int n) {
-        unordered_set<int> ans;
-        int sum;
+        unordered_set<int> s;
+        int sum=0;
         while(1){
             sum=getsum(n);
-            if(sum==1){
-                return true;
-            }
-            if(ans.find(sum)!=ans.end()){
+            if(sum==1)  return true;
+            else if(s.find(sum)!=s.end()){
                 return false;
             }
             else{
-                ans.insert(sum);
+                s.insert(sum);
             }
             n=sum;
         }
@@ -31,5 +29,5 @@ public:
 用 unordered_set 來儲存每次位元相乘的計算結果
 因為有可能會算到重複的，所以需要用一個 hash table(set) 來儲存結果
 如果等於 1 就回傳 true
-發現有相同的就回傳false
+發現set裡面有相同的值就回傳false(代表陷入迴圈)
 */
