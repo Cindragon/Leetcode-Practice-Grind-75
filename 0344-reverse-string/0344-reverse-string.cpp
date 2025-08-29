c226478377ac068f92ee3ed753ed84c7cec1056a
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-08-29 15:56:51 +0800

    Time: 0 ms (100%), Space: 27.4 MB (16.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int j=s.size()-1;
-        for(int i=0;i<s.size()/2;i++){
+        for(int i=0, j=s.size()-1;i<s.size()/2;i++, j--){
             char temp=s[i];
             s[i]=s[j];
             s[j]=temp;
-            j--;
         }
     }
 };
