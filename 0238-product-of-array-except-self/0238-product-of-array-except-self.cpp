bc0cd1fc355b3c565abf9eda8234e29f4bc17335
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-09-01 10:37:27 +0800

    Time: 3 ms (37.46%), Space: 41.2 MB (39.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans, prefix, suffix;
+        int n=nums.size();
+        vector<int> prefix(n), ans(n);
         int product=1;
         for(int i=0;i<nums.size();i++){
-            ans.push_back(product);
+            prefix[i]=product;
             product*=nums[i];
         }
         product=1;
-        for(int i=nums.size()-1;i>=0;i--){
-            ans[i]*=product;
+        for(int i=prefix.size()-1;i>=0;i--){
+            ans[i]=product*prefix[i];
             product*=nums[i];
         }
         return ans;
     }
 };
+
+/*
+建立兩個陣列：
+    prefix：儲存每個位置左邊所有數字的乘積
+    ans：儲存最終答案
+
+第一次從前往後遍歷：
+    prefix[i] = nums[0] * nums[1] * ... * nums[i-1]
+    等於「當前位置左邊的乘積」
+
+第二次從後往前遍歷：
+    用一個變數 product 紀錄右邊所有數字的乘積
+    將左邊的乘積與右邊的乘積相乘，最後輸出 ans
+*/
