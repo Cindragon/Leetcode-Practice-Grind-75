a21d89bfea6f015c84b50c84eeff2c74df4a6122
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-09-11 22:41:29 +0800

    Time: 0 ms (100%), Space: 16.2 MB (62.32%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -10,7 +10,6 @@ public:
         for(int i=1;i<n;i++){
             dp[0][i]=dp[0][i-1]+grid[0][i];
         }
-        int res=INT_MAX;
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 dp[i][j]=min(dp[i-1][j], dp[i][j-1])+grid[i][j];
