bd3f17e357689e94af46f97e1066cafb18d849d8
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-09-12 16:12:13 +0800

    Time: 2 ms (57.57%), Space: 17.9 MB (58.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    class mycomparison{
-    public:
-        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs){
-            return lhs.second>rhs.second;
-        }
-    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans(k);
         unordered_map<int, int> mp;
-        for(const auto&n:nums){
-            mp[n]++;
+        for(const auto&i:nums){
+            mp[i]++;
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;
+        auto cmp=[](const pair<int, int>&a, const pair<int, int>&b){
+            return a.second>b.second;
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pri_que(cmp);
         for(const auto&it:mp){
             pri_que.push(it);
             if(pri_que.size()>k){
                 pri_que.pop();
             }
         }
-        vector<int> ans(k);
         for(int i=k-1;i>=0;i--){
             ans[i]=pri_que.top().first;
             pri_que.pop();
         }
-    return ans;
+        return ans;
     }
 };
+
 /*
 先用 map 來遍歷數組中的元素頻率
-接下來用最小堆(Min-Heap)來對各元素出現的頻率做統計 (priority_que)
-在迴圈中遍歷 map，將map的元素 push 進去pri_que中
+接下來用最小堆(Min-Heap)來對各元素出現的頻率做統計 (透過 priority_que 來設計)
+把出現頻率從少排到多，並且透過 lambda 寫 cmp 的比較函式來做排序(不需要再多一個class)
+在迴圈中遍歷 map，將 map 的元素 push 進去 pri_que 中
 若是pri_que的大小>k，就將最頂端的元素(出現頻率最少的元素) pop 出去
 最後用 vector 儲存pri_que.top().first()，然後再pop()
 */
