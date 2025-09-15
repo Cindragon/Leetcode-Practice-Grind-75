4fc200df34545cbf052162e4fdbe236d95b14a91
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-09-15 23:30:25 +0800

    Time: 0 ms (100%), Space: 18 MB (78.71%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        for(const auto&p:prerequisites){
+            int a=p[0], b=p[1];
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;++i){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int t=q.front();
+            q.pop();
+            ans.push_back(t);
+            for(int a:adj[t]){
+                indegree[a]--;
+                if(indegree[a]==0){
+                    q.push(a);
+                }
+            }
+        }
+        if(ans.size()==n)   return ans;
+        else return {};
+    }
+};
+
+/*
+跟 LeetCode 207 很像，但是是要找到修課順序，把修課順序 push_back 到 ans ，最後回傳ans陣列
+採用 BFS 的方式來解題(Kahn（BFS，入度法）)，用到拓撲排序的概念。
+1. 首先用
+    一個二維陣列 adj (鄰接表)，adj[i] 代表從課程 i 出發可以修的後續課程， 
+    n 代表課程總數
+    透過indegree[i] 記錄每門課的入度（還有多少先修課沒修）。
+
+2. 接下來遍歷 prerequisite 陣列，將每個課程所需要的先修課程記錄下來並且計算入度
+    對每個 [a,b]，表示「修 a 前必須先修 b」
+    在圖中加入邊 b -> a
+
+3. 透過 queue 陣列將入度為 0 的課程 push 進 queue 中（代表可以第一時間先修的課程）
+
+4. 用 ans 記錄課堂數，在 queue 不為空的情況下，將裡面的課堂代號 t 拿出來，並且 ans++
+
+5. BFS 遍歷：
+   - 從 queue 拿出課程 t，ans.push_back(t)
+   - 遍歷 t 的後續課程 a：
+       - 將 indegree[a]--（表示已完成一個先修課）
+       - 如果 indegree[a] == 0，代表 a 現在可以修了，放入 queue
+
+6. 回傳 ans 
+*/
