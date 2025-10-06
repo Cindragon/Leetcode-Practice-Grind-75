9b3d0b4be650f84b0d0f342e0029ef4d3783c72d
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-10-06 23:04:38 +0800

    Time: 0 ms (100%), Space: 28 MB (48.35%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -10,19 +10,20 @@
  * };
  */
 class Solution {
-public:
-    int dfs(TreeNode* root, int& s){
-        if(root==NULL) return 0;
-        int leftSum=max(dfs(root->left, s), 0);
-        int rightSum=max(dfs(root->right, s), 0);
+private:
+    int traversal(TreeNode* root, int& s){
+        if(root==nullptr)   return 0;
+        int leftSum=max(traversal(root->left, s), 0);
+        int rightSum=max(traversal(root->right, s), 0);
         int currSum=root->val+leftSum+rightSum;
         s=max(s, currSum);
         return root->val+max(leftSum, rightSum);
     }
+public:
     int maxPathSum(TreeNode* root) {
-        int s=INT_MIN;
-        dfs(root, s);
-        return s;
+        int ans=INT_MIN;
+        traversal(root, ans);
+        return ans;
     }
 };
 
@@ -34,8 +35,8 @@ public:
 單層遞迴邏輯：
     左邊的和= 左子樹的值與 0 比較大小(如果是負數就不要加上去)
     右邊的和同理
-    接著處理中間的 currSum=左邊加上右邊再加上自己root->val
-    s 進行更新，取 s 跟 currSum 的最大值
+    接著處理中間的 currSum= 左邊加上右邊再加上自己 root->val
+    s 進行更新，取 s 跟 currSum 的最大值(一整條路徑的最大值)
     最後要回傳的是 root->val 加上左或右的最大值
     (回傳給上一層時，只能取「單邊路徑」，不能取左右兩邊合併，因為這樣就不能繼續向上延伸。)
 才能得到整條路徑總和的最大值
