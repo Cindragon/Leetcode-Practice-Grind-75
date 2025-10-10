3f77547d562bfc47a50a2427a469f1f3092a75b8
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-10-11 00:03:34 +0800

    Time: 7 ms (24.3%), Space: 24.1 MB (23.51%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,18 +1,34 @@
 class NumArray {
 private:
-    vector<int> prefixSum;
+    vector<int> segTree;
+    int n;
+    void buildTree(vector<int>&nums, int idx, int l, int r){
+        if(l==r){
+            segTree[idx]=nums[l];
+            return;
+        }
+        int mid=l+(r-l)/2;
+        buildTree(nums, idx*2+1 ,l, mid);
+        buildTree(nums, idx*2+2, mid+1, r);
+        segTree[idx]=segTree[idx*2+1]+segTree[idx*2+2];
+    }
+    int queryRange(int idx, int l, int r, int ql, int qr){
+        if(qr<l || ql>r)    return 0;
+        if(ql<=l && r<=qr)  return segTree[idx];
+        int mid=l+(r-l)/2;
+        return queryRange(idx*2+1, l, mid, ql, qr)
+               +queryRange(idx*2+2, mid+1, r, ql, qr);
+    }
 public:
     NumArray(vector<int>& nums) {
-        int n=nums.size();
-        prefixSum.resize(n+1);
-        prefixSum[0]=0;
-        for(int i=1;i<=n;++i){
-            prefixSum[i]=prefixSum[i-1]+nums[i-1];
-        }
+        n=nums.size();
+        if(n==0)    return;
+        segTree.resize(4*n);
+        buildTree(nums, 0, 0, n-1);
     }
     
     int sumRange(int left, int right) {
-        return prefixSum[right+1]-prefixSum[left];
+        return queryRange(0, 0, n-1, left, right);
     }
 };
 
