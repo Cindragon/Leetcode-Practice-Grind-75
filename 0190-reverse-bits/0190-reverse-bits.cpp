07f137fb5bd01f7e9e9386940c378b6b47084bfd
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-10-17 23:19:16 +0800

    Time: 4 ms (28.46%), Space: 8 MB (92.29%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,7 +4,7 @@ public:
         int ans=0;
         for(int i=0;i<32;++i){
             ans<<=1;    //ans往左移一位
-            ans|=(n&1); //ans 跟 n 最左邊的一位 bit 做 or
+            ans|=(n&1); // 取出 n 的「最右邊」那一位，放到 ans 的最低位
             n>>=1;  //n 往右移一位
             //ans = (ans << 1) | ((n >> i) & 1);
         }
