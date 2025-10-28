e79b7999b55c9843a405cd7ec0a707ada3beab90
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-10-28 17:47:24 +0800

    Time: 0 ms (100%), Space: 14.2 MB (28.12%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,28 @@
 class Solution {
-public:
+private:
+    vector<vector<int>> ans;
     vector<int> path;
-    vector<vector<int>> res;
-    void backtracking(vector<int>& nums, int target, int startindex){
+    void backtracking(vector<int>& candidates, int target, int startIndex){
         if(target==0){
-            res.push_back(path);
+            ans.push_back(path);
             return;
         }
-        for(int i=startindex;i<nums.size();i++){
-            if(i>startindex&&nums[i]==nums[i-1]) continue;
-            if(nums[i]>target) break;
-            path.push_back(nums[i]);
-            backtracking(nums, target-nums[i], i+1);
+        for(int i=startIndex;i<candidates.size();++i){
+            if(i>startIndex && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target)    break;
+            path.push_back(candidates[i]);
+            backtracking(candidates, target-candidates[i], i+1);
             path.pop_back();
         }
+        return;
     }
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         backtracking(candidates, target, 0);
-        return res;
+        return ans;
     }
 };
-
 /*
 跟 Leetcode 39 一樣的題型，只是現在數字不能重複使用
 但是在迴圈中要再做剪枝
