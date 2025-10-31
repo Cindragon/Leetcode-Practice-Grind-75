4c9ad8e9fed7a12f2d002c25c2b6ce61efe54106
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-10-31 15:21:28 +0800

    Time: 0 ms (100%), Space: 11 MB (29.32%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,42 +1,44 @@
 class Solution {
-public:
+private:
+    vector<vector<int>> ans;
     vector<int> path;
-    vector<vector<int>> res;
     void backtracking(vector<int>& nums, vector<bool>& used){
         if(path.size()==nums.size()){
-            res.push_back(path);
+            ans.push_back(path);
             return;
         }
         for(int i=0;i<nums.size();i++){
-            if(used[i]) continue;   //path裡面已經有的元素直接跳過
+            if(used[i]) continue;
             used[i]=true;
             path.push_back(nums[i]);
             backtracking(nums, used);
             path.pop_back();
             used[i]=false;
         }
+        return;
     }
+public:
     vector<vector<int>> permute(vector<int>& nums) {
-        path.clear();
-        res.clear();
-        vector<bool> used(nums.size(), false);
+        int n=nums.size();
+        vector<bool> used(n, false);
         backtracking(nums, used);
-        return res;
+        return ans;
     }
 };
+
 
 /*
 排列問題
 排列與組合不同的地方在於：
     排列的元素順序是有差別的([1,2]跟[2,1]有差)，而組合的話則沒有差別([1,2]跟[2,1]沒差)
 這題也是一樣用回溯算法來解
-相較於組合問題，這題使用used數組來解決
+相較於組合問題，這題使用 used 數組來解決(看哪個元素有被用過)
 傳入參數：nums 數組跟 used 數組
-終止條件：path的大小等於nums的大小，將path加入res後return
+終止條件：path 的大小等於 nums 的大小，將 path 加入 ans 後return
 單層遞迴邏輯：
     如果(used[i])，代表這個元素被用過了，直接continue
     然後將used[i]設為true
     path加入nums[i]
     接下來用backtracking跟回溯的方式還原
-最後回傳res
+最後回傳 ans
 */
