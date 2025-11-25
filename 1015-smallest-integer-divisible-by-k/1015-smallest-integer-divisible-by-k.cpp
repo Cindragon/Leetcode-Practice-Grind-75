c51676c8013deb0e0f05459cec4ccf423fb91c53
Tsai Chen Hung <114350612+Cindragon@users.noreply.github.com>
2025-11-25 18:40:34 +0800

    Time: 2 ms (29.51%), Space: 7.8 MB (52.58%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -7,10 +7,15 @@ public:
         while(remainder!=0){
             remainder=(10*remainder+1)%k;
             ans+=1;
+            if(ans>k+1) return -1;
         }
         return ans;
     }
 };
 
 /*
+在找的時候不要想著建立整個數列(1, 11, 111, 1111......)
+而是從餘數去找(這樣remainder永遠都會在1~k-1之間)
+而餘數最多就是k+1個，所以運算次數>k+1時，代表進入循環
+直接回傳 -1(代表除不盡)，否則回傳ans
 */
